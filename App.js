5c91d1d6a9393915dc5caee6f54391149c8ebbe3
dov11 <ovo.dan@gmail.com>
2018-01-10 11:43:22 +0100

    add routes

--- App.js
+++ App.js
@@ -1,48 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { Router, Scene } from 'react-native-router-flux';
+import store from './store';
+import SignIn from './screens/SignIn';
 import SignUp from './screens/SignUp';
+// import ChatRoom from './screens/ChatRoom';
+// import styles from './App.styles';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
     return (
-      <SignUp />
+      <Provider store={store}>
+        <Router>
+          <Scene key="root">
+            <Scene key="signIn" component={SignIn} title="Sign In" initial={true} />
+            <Scene key="signUp" component={SignUp} title="Sign Up" />
+          </Scene>
+        </Router>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#00FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#00FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
--- store.js
+++ store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import ReduxThunk from 'redux-thunk'
 import { composeWithDevTools } from 'remote-redux-devtools';
 import apiMiddleware from './middleware/api';
-import * as reducers from './reducers'
+import reducers from './reducers'
 
 const reducer = combineReducers(Object.assign({}, reducers))
 
